437c214315d2b97543cc023a948813e534a2de73
projectm4n <107320326+projectm4n@users.noreply.github.com>
2022-10-21 19:57:04 +0700

    update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,58 +1,56 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
-# Copyright (C) 2021 TeamUltroid for autobot
-# Recode by @mrismanaziz
-# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
 # t.me/SharingUserbot & t.me/Lunatic0de
-#
-""" Userbot start point """
+
+import importlib
+
+from pyrogram import idle
+from uvloop import install
+
+from config import BOT_VER, CMD_HANDLER
+from ProjectMan import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
+from ProjectMan.helpers.misc import create_botlog, heroku
+from ProjectMan.modules import ALL_MODULES
+
+MSG_ON = """
+🔥 **PyroMan-Userbot Berhasil Di Aktifkan**
+━━
+➠ **Userbot Version -** `{}`
+➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
+━━
+"""
 
 
-import sys
-from importlib import import_module
-from platform import python_version
+async def main():
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"ProjectMan.modules.{all_module}")
+    for bot in bots:
+        try:
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("Lunatic0de")
+            await bot.join_chat("jmthon")
+            await bot.join_chat("Y88F8")
+            await bot.join_chat("R125R")
+            try:
+                await bot.send_message(
+                    BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
+                )
+            except BaseException:
+                pass
+            LOGGER("ProjectMan").info(
+                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
+            )
+        except Exception as a:
+            LOGGER("main").warning(a)
+    LOGGER("ProjectMan").info(f"PyroMan-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
+        await create_botlog(bot1)
+    await idle()
+    await aiosession.close()
 
-from pytgcalls import __version__ as pytgcalls
-from telethon import version
-
-from userbot import BOT_TOKEN
-from userbot import BOT_VER as ubotversion
-from userbot import BOTLOG_CHATID, LOGS, LOOP, bot
-from userbot.clients import man_userbot_on, multiman
-from userbot.core.git import git
-from userbot.modules import ALL_MODULES
-from userbot.utils import autobot, autopilot
-
-try:
-    for module_name in ALL_MODULES:
-        imported_module = import_module(f"userbot.modules.{module_name}")
-    client = multiman()
-    total = 5 - client
-    git()
-    LOGS.info(f"Total Clients = {total} User")
-    LOGS.info(f"Python Version - {python_version()}")
-    LOGS.info(f"Telethon Version - {version.__version__}")
-    LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
-    LOGS.info(f"Man-Userbot Version - {ubotversion} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
-except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
-    pass
-except BaseException as e:
-    LOGS.info(str(e), exc_info=True)
-    sys.exit(1)
-
-
-LOOP.run_until_complete(man_userbot_on())
-if not BOTLOG_CHATID:
-    LOOP.run_until_complete(autopilot())
-if not BOT_TOKEN:
-    LOOP.run_until_complete(autobot())
-if len(sys.argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    try:
-        bot.run_until_disconnected()
-    except ConnectionError:
-        pass
